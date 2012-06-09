aff69ec9b4917450be213444d9d58a683504875b
Mark Ryall <mark@ryall.name>
2012-06-09 19:21:38 +1000

    added player and parsing artist and track

--- lib/scrobble_somafm.rb
+++ lib/scrobble_somafm.rb
@@ -1,5 +1,22 @@
 module ScrobbleSomafm
   def self.start args
-    puts 'starting'
+    begin
+      play 'secretagent' do |artist, track|
+        puts "Now Playing: #{track} by #{artist}"
+      end
+    rescue Interrupt
+    end
+  end
+
+  def self.play channel
+    IO.popen "mplayer -quiet -vo none -ao coreaudio http://somafm.com/startstream=#{channel}.pls 2>&1" do |io|
+      while line = io.gets
+        parse(line) { |artist, track| yield artist, track }
+      end
+    end
+  end
+
+  def self.parse line
+    yield $1, $2 if line =~ /ICY Info: StreamTitle='(.*) - (.*)';StreamUrl='(.*)';/
   end
 end
