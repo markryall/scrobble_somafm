888415b99cbaa87553ff3fb043da35ead92f4a8f
Mark Ryall <mark@ryall.name>
2012-06-09 19:32:07 +1000

    added search for several of the somafm channels

--- lib/scrobble_somafm.rb
+++ lib/scrobble_somafm.rb
@@ -1,14 +1,21 @@
 module ScrobbleSomafm
   def self.start args
     begin
-      play 'secretagent' do |artist, track|
+      play args.shift do |artist, track|
         puts "Now Playing: #{track} by #{artist}"
       end
     rescue Interrupt
     end
   end
 
-  def self.play channel
+  def self.find_channel text
+    %w{secretagent doomed groovesalad illstreet indiepop lush sonicuniverse spacestation}.grep(/#{text}/).first
+  end
+
+  def self.play text
+    channel = find_channel text    
+    abort "Unknown channel #{text}" unless channel
+    puts "Playing channel #{channel}"
     IO.popen "mplayer -quiet -vo none -ao coreaudio http://somafm.com/startstream=#{channel}.pls 2>&1" do |io|
       while line = io.gets
         parse(line) { |artist, track| yield artist, track }
@@ -18,5 +25,10 @@ module ScrobbleSomafm
 
   def self.parse line
     yield $1, $2 if line =~ /ICY Info: StreamTitle='(.*) - (.*)';StreamUrl='(.*)';/
+  end
+
+  def self.abort message
+    puts message
+    exit 0
   end
 end
